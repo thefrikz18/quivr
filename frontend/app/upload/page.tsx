6681e3209a0cdaf18be0f9adc71b37a0d91ad5d7
Luis Eduardo Mendez Tafolla <luismendeztaf3@gmail.com>
2023-08-01 14:13:49 -0600

    Se quito boton de chat y website url @thefrikz18

--- frontend/app/upload/page.tsx
+++ frontend/app/upload/page.tsx
@@ -1,11 +1,7 @@
 "use client";
-import Link from "next/link";
 
-import Button from "@/lib/components/ui/Button";
-import { Divider } from "@/lib/components/ui/Divider";
 import PageHeading from "@/lib/components/ui/PageHeading";
 
-import { Crawler } from "./components/Crawler";
 import { FileUploader } from "./components/FileUploader";
 
 const UploadPage = (): JSX.Element => {
@@ -16,15 +12,6 @@ const UploadPage = (): JSX.Element => {
         subtitle="Text, document, spreadsheet, presentation, audio, video, and URLs supported"
       />
       <FileUploader />
-      <Divider text="or" className="m-5" />
-      <Crawler />
-      <div className="flex flex-col items-center justify-center gap-5 mt-5">
-        <Link href={"/chat"}>
-          <Button variant={"secondary"} className="py-3">
-            Chat
-          </Button>
-        </Link>
-      </div>
     </main>
   );
 };
